b0e37bfcb0ae1039deaeafc6d812f5cdbde35354
LiXiangmin <2329716529@qq.com>
2026-04-26 23:34:12 +0800

    模型参数配置

--- langchain4j-05model-parameters/src/main/java/com/atguigu/study/controller/ModelParameterController.java
+++ langchain4j-05model-parameters/src/main/java/com/atguigu/study/controller/ModelParameterController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
 
 /**
  * @auther zzyybs@126.com
